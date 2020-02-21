d5a6a24940df9e18ebf13885e5b411c7cc9427a3
Cassie <cassie.gu@mkcorp.com>
2020-02-21 11:48:23 +0800

    Init proj

---
+++ TestSonarSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSonarSwift
+//
+//  Created by Cassie on 2020/2/21.
+//  Copyright © 2020 Cassie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
